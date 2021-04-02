67c6b0b32bea67840c35ad2e7c8539e57746d435
Eduardo Luz <eduluz1976@gmail.com>
2021-04-01 22:08:28 -0700

    final solution

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function compute() {
+    principal = parseInt(document.getElementById("principal").value);
+    rate = parseFloat(document.getElementById("rate").value);
+    period = parseInt(document.getElementById("years").value);
+
+
+
+    var amount = period * principal * ((rate / 100));
+
+    var finalYear = parseInt((new Date()).getFullYear()) + parseInt(period);
+
+
+
+    listMappedFields = {
+        value_principal: principal,
+        value_rate: rate,
+        value_amount: parseInt(amount),
+        value_final_year: finalYear
+    };
+
+    for (k in listMappedFields) {
+        document.getElementById(k).innerText = listMappedFields[k];
+    }
+
+    document.getElementById("result").style.display = "block";
+
 }
-        
+
+function initPage() {
+    var yearsContainer = document.getElementById("years");
+    for (var i = 1; i <= 30; i++) {
+        var elYear = document.createElement("option");
+        elYear.setAttribute("value", i);
+        elYear.innerText = i;
+        yearsContainer.appendChild(elYear);
+    }
+    updateRate();
+}
+
+
+function updateRate(value) {
+    var value = document.getElementById('rate').value
+    document.getElementById('display_rate').innerHTML = value
+}
+
